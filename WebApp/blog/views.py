8c70494b9da082d3ce8e40f277612f523906757f
bgz30 <44124521+BGZ30@users.noreply.github.com>
2020-06-30 10:43:27 +0200

    fixed html tag

--- WebApp/blog/views.py
+++ WebApp/blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+#HTTP response
+from django.http import HttpResponse
+
+# handle the traffic
+def home(requset):
+    return HttpResponse('<h1>Blog home</h1>')
+
