1b885d1055bfa614bf0dc34c747eb98208e04cc5
bgz30 <44124521+BGZ30@users.noreply.github.com>
2020-06-30 10:53:57 +0200

    adde the blog-About url

--- WebApp/blog/views.py
+++ WebApp/blog/views.py
@@ -4,6 +4,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # handle the traffic
+
+# Home page
 def home(requset):
     return HttpResponse('<h1>Blog home</h1>')
 
+# hAbout page
+def about(requset):
+    return HttpResponse('<h1>Blog about</h1>')
