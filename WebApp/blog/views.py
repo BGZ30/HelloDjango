1eca115bbb21691d9b5701cd3e23bbc54957a9e5
bgz30 <44124521+BGZ30@users.noreply.github.com>
2020-06-30 15:34:11 +0200

    added some dummy data and pass it to template

--- WebApp/blog/views.py
+++ WebApp/blog/views.py
@@ -5,10 +5,32 @@ from django.shortcuts import render
 
 # handle the traffic
 
+# Adding some dummy data
+posts = [
+    {
+        'author': 'Bassant',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'June 30, 2020'
+    },
+    {
+        'author': 'Maisa',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'June 30, 2020'
+    }
+]
+
 # Home page
 def home(request):
+
+    """ pass the data to the template;
+        create a dictionary with a key called posts, and values are the dummy data 'posts'
+    """
+    context = {'posts': posts}
+
     # return HttpResponse('<h1>Blog home</h1>')
-    return render(request, 'blog/home.html')
+    return render(request, 'blog/home.html', context)
 
 # hAbout page
 def about(request):
