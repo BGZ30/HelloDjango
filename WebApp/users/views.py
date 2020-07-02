508fcaf37d34d55cb4454aa82c8f7ef3acd8efe1
bgz30 <44124521+BGZ30@users.noreply.github.com>
2020-07-02 22:16:45 +0200

    updated after crispy template

--- WebApp/users/views.py
+++ WebApp/users/views.py
@@ -1,6 +1,23 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+# from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages  # to show a flash message
+from .forms import UserRegisterForm
+
 
 def register(request):
-    form = UserCreationForm()
+    if request.method == 'POST':
+        #form = UserCreationForm(request.POST)
+        form = UserRegisterForm(request.POST)
+
+        """ if the form is valid, info is stored in cleaned_data dicd"""
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account Created For {username}')
+
+            return redirect('blog-home')
+    else:
+        #form = UserCreationForm()
+        form = UserRegisterForm()
+
     return render(request, 'users/register.html', {'form': form})
