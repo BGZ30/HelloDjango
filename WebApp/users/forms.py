ebd9b300279544913e15e0a5a6cc57c9e14b6203
bgz30 <44124521+BGZ30@users.noreply.github.com>
2020-07-05 09:00:29 +0200

    added forms to update profile

--- WebApp/users/forms.py
+++ WebApp/users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
@@ -12,3 +13,18 @@ class UserRegisterForm(UserCreationForm):
 
         # fields shown on the form
         fields = ['username', 'email', 'password1', 'password2']
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    # specify the model that this form is gonna interact with
+    class Meta:
+        # if the form is valid, then it will create a user
+        model = User
+
+        # fields shown on the form
+        fields = ['username', 'email']
+
+class ProfileUpdateForm(form.ModelForm):
+    model = Profile
+    fields = ['image']
