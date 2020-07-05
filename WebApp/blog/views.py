caa5a25cb409d03814109c31f8a2e91d0c1fc6c0
bgz30 <44124521+BGZ30@users.noreply.github.com>
2020-07-05 19:32:57 +0200

    updated for detailVeiw

--- WebApp/blog/views.py
+++ WebApp/blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 #HTTP response
@@ -24,7 +24,9 @@ class PostListView(ListView):
     template_name = 'blog/home.html'  # <app>/<model>_<viewtype>.html
     context_object_name = 'posts'
     ordering = ['-date_posted'] # order posts by date desc
-    
+
+class PostDetailView(DetailView):
+    model = Post  
 
 # hAbout page
 def about(request):
--- WebApp/users/models.py
+++ WebApp/users/models.py
@@ -8,7 +8,7 @@ class Profile(models.Model):
     CASCADE: if the user is deleted--> delete the profile "BUT" if the profile is deleted DON'T delete the user 
     """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    image = models.ImageField(default='default.png', upload_to='profile_pics')
 
     def __str__(self):
         return f'{self.user.username} Profile' 
