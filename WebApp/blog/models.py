86da8af8823f1eb46e4dcf52165dd78ad80df107
bgz30 <44124521+BGZ30@users.noreply.github.com>
2020-07-10 23:00:02 +0200

    fixed delete tag

--- WebApp/blog/models.py
+++ WebApp/blog/models.py
@@ -17,4 +17,4 @@ class Post(models.Model):
         return self.title
     
     def get_absolute_url(self):
-        return reverse('post-detail', kw)
+        return reverse('post-detail', kwargs={'pk': self.pk})
