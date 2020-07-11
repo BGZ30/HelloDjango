8885cdfa550aad8d65b0a6e7d7caa4aa31eec295
bgz30 <44124521+BGZ30@users.noreply.github.com>
2020-07-11 14:42:37 +0200

    add paginator

--- WebApp/blog/views.py
+++ WebApp/blog/views.py
@@ -25,6 +25,7 @@ class PostListView(ListView):
     template_name = 'blog/home.html'  # <app>/<model>_<viewtype>.html
     context_object_name = 'posts'
     ordering = ['-date_posted'] # order posts by date desc
+    paginate_by = 5
 
 class PostDetailView(DetailView):
     model = Post
